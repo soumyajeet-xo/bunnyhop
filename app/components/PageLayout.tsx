3ce0dd69a9a34eb807afdac22e2074dec53b79eb
Soum <tecsoumyajeet@gmail.com>
2025-07-08 17:24:41 +0530

    top bar created gor global context

--- app/components/PageLayout.tsx
+++ app/components/PageLayout.tsx
@@ -14,6 +14,7 @@ import {
   SearchFormPredictive,
 } from '~/components/SearchFormPredictive';
 import {SearchResultsPredictive} from '~/components/SearchResultsPredictive';
+import TopBar from '~/components/sections/TopBar';
 
 interface PageLayoutProps {
   cart: Promise<CartApiQueryFragment | null>;
@@ -37,6 +38,7 @@ export function PageLayout({
       <CartAside cart={cart} />
       <SearchAside />
       <MobileMenuAside header={header} publicStoreDomain={publicStoreDomain} />
+      <TopBar />
       {header && (
         <Header
           header={header}
---
+++ app/components/sections/TopBar.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function TopBar() {
+  return (
+    <div className="w-full bg-black text-white text-center py-2 text-xs md:text-sm font-medium tracking-wider uppercase">
+      Free Standard Shipping in the US on orders over $99
+    </div>
+  );
+} 
